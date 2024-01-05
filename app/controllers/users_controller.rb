9dbc3d5dcf601a07d9dcc956deaf5966c47aac32
asagohan2301 <shemomechama@yahoo.co.jp>
2024-01-05 21:11:02 +0900

    ログインエンドポイントを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,31 @@ class UsersController < ApplicationController
     end
   end
 
+  def login
+    user = User.find_by(email: login_params[:email])
+    if user && user.authenticate(login_params[:password])
+      payload = { id: user.id }
+      secret = "test_secret_key"
+      token = JWT.encode payload, secret, "HS256"
+      data = {
+        user: {
+          username: user.username,
+          token: token
+        }
+      }
+      render json: data, status: :ok
+    else
+      render json: { error: "Authentication failed." }, status: :unauthorized
+    end
+  end
+
   private
   
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def login_params
+      params.require(:user).permit(:email, :password)
     end
 end
