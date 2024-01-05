ee5325ba23e69464080d82c1cc6c3a02cfb963e5
asagohan2301 <shemomechama@yahoo.co.jp>
2024-01-05 17:21:06 +0900

    ユーザー登録のエンドポイントを実装しました

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      data = {
+        user: {
+          username: user.username,
+          email: user.email
+        }
+      }
+      render json: data, status: :created
+    else
+      error = {
+        messages: user.errors.full_messages
+      }
+      render json: error, status: :unprocessable_entity
+    end
+  end
+  
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
