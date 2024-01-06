06ce764fb6b3ed5676728eac49e768be725dde58
asagohan2301 <shemomechama@yahoo.co.jp>
2024-01-06 14:13:13 +0900

    認証機能のメソッドの分離と、エラー処理追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,19 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user
+
   def create
-    secret = "test_secret_key"
-    token = request.headers['Authorization'].split(" ").last
-    decoded_token = JWT.decode token, secret, true, { algorithm: 'HS256' }
-
-    id = 0
-    decoded_token.each do |h|
-      id = h["id"] if h.key?("id")
-    end
-
-    user = User.find(id)
-    article = user.articles.build(article_params)
-  
+    article = @user.articles.build(article_params)
     if article.save
       data = {
         article: {
           id: article.id,
-          title: article.title
+          title: article.title,
+          description: article.description,
+          body: article.body,
+          tag_list: article.tag_list,
+          created_at: article.created_at,
+          updated_at: article.updated_at,
+          author: @user.username
         }
       }
       render json: data, status: :created
@@ -25,7 +22,22 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def article_params
-    params.require(:article).permit(:title, :description, :body, tag_list: [])
-  end
+  private
+
+    def authenticate_user
+      begin
+        secret = "test_secret_key"
+        token = request.headers['Authorization'].split(" ").last
+        decoded_token = JWT.decode token, secret, true, { algorithm: 'HS256' }
+        id_hash = decoded_token.find { |h| h.key?("id") }
+        @user = User.find(id_hash["id"])
+      rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+        render json: { error: "Invalid token or user not found." }, status: :unauthorized
+        return
+      end
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :description, :body, tag_list: [])
+    end
 end
